c9528a8edbee0378ec523b64eebbbc5855a2d03b
administrator <administrator@vinzor.(none)>
2018-10-13 17:17:52 +0800

    first edition

--- main.cpp
+++ main.cpp
@@ -4,4 +4,10 @@
 int main() {
 	Date a("2014-02-13/12:33");
 	cout << a.dateToString() << endl;
+	vector<string> b;
+	b.push_back("ajkl");
+	Meeting meet("asdf",b,a,a,"hello world");
+	cout << meet.getSponsor() << endl;
+	cout << meet.getTitle() << endl;
+	cout << meet.getStartDate().dateToString() << endl;
 }
